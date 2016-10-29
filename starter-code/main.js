1f949d2047260b25f5522bbce4bfbca33efb27bb
philip frazer <philipfrazer@gmail.com>
2016-10-29 14:43:02 -0400

    Alerts added to JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,13 @@
 
-var cardOne = "queen";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 
-var cardTwo = "queen";
-
-var cardThree = "king";
-
-var cardFour = "king";
+if (cardThree === cardFour) {
+  alert('You found a match!');	
+} else if (cardOne === cardTwo) {
+  alert('You found a match!');
+} else {
+  alert ('Sorry, try again');
+}
