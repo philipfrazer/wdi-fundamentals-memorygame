2b717c89e67ef26259a7e5f723bedf58cb21a793
philip frazer <philipfrazer@gmail.com>
2016-10-30 16:36:47 -0400

    Added card creating fucntion in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,29 @@
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+// var cardOne = 'queen';
+// var cardTwo = 'queen';
+// var cardThree = 'king';
+//var cardFour = 'king';
 
-if (cardThree === cardFour) {
-  alert('You found a match!');	
-} else if (cardOne === cardTwo) {
-  alert('You found a match!');
-} else {
-  alert ('Sorry, try again');
+// if (cardThree === cardFour) {
+//  alert('You found a match!');	
+// } else if (cardOne === cardTwo) {
+//  alert('You found a match!');
+// } else {
+//  alert ('Sorry, try again');
+//}
+
+var board = document.getElementById('game-board');
+
+function createCards = function() {
+
+	for(var i=0; i<cards.length; i++) {
+
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+
+		board.appendChild(cardElement);
+  }
+
 }
+
